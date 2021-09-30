6de5ead0c2ff248a266f4d28482dc983fe442ac2
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2021-09-30 17:42:24 +0530

    Day 1

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ connectToMongo();
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-  res.send('Hello Tawan!')
-})
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })

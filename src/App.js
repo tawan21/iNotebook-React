e9c4c7dc1f9d804e91a94102e20fddba26b3b733
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2021-10-07 17:07:41 +0530

    Day 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
